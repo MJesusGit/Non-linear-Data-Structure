78b1a3186c23270bddd63b63fbdc2b4c4651f70a
mjpllano <72667996+MJesusGit@users.noreply.github.com>
2021-11-25 18:54:20 +0100

    some files

---
+++ 04_graphs(CODE)/DecoratedElement.java
@@ -0,0 +1,26 @@
+package graph;
+
+import graphsDSESIUCLM.*;
+public class DecoratedElement<T> implements Element{
+	private int ID;
+	private T name;
+	private int value;
+	private boolean visited;
+	private DecoratedElement<T> parent;
+	@Override
+	public String getID() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+		
+	
+	
+
+	
+	
+	
+	
+	
+	
+}
