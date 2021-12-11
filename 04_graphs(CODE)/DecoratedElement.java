084b5b5b9ba89b28a0ab80526ce2cd2aa678554b
Juan Gigante Ríos <91609579+jgiganterios@users.noreply.github.com>
2021-12-11 12:23:30 +0100

    Add files via upload

--- 04_graphs(CODE)/DecoratedElement.java
+++ 04_graphs(CODE)/DecoratedElement.java
@@ -1,14 +1,14 @@
-package graph;
+package graphs;
 
 import graphsDSESIUCLM.*;
 
-public class DecoratedElement<Character> implements Element{
+public class DecoratedElement<Character> implements Element {
 	private String ID;
 	private Character element;
 	private boolean visited;
 	private DecoratedElement<Character> parent;
 	private int distance;
-	
+
 	public DecoratedElement(String key, Character element) {
 		this.element = element;
 		ID = key;
@@ -16,35 +16,50 @@ public class DecoratedElement<Character> implements Element{
 		parent = null;
 		distance = 0;
 	}
+
 	public void setID(String iD) {
 		ID = iD;
 	}
+
 	@Override
 	public String getID() {
 		return ID;
 	}
+
 	public Character getElement() {
 		return element;
 	}
+
 	public void setElement(Character element) {
 		this.element = element;
 	}
+
 	public boolean isVisited() {
 		return visited;
 	}
+
 	public void setVisited(boolean visited) {
 		this.visited = visited;
 	}
+
 	public DecoratedElement<Character> getParent() {
 		return parent;
 	}
+
 	public void setParent(DecoratedElement<Character> parent) {
 		this.parent = parent;
 	}
+
 	public int getDistance() {
 		return distance;
 	}
+
 	public void setDistance(int distance) {
 		this.distance = distance;
+	}
+
+	public boolean equals(Object n) {
+		return (ID.equals(((DecoratedElement) n).getID())
+				&& element.equals(((DecoratedElement<Character>) n).getElement()));
 	}
 }
